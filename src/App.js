85fb23370a367131b8f49bdc9ff5864b62d0c662
mwhitehat <manua1712890@gmail.com>
2026-02-02 21:18:25 +0100

    Finalize portfolio and prepare for deployment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { NavBar } from './components/NavBar'
+import { Banner } from './components/Banner';
+import {Skills} from './components/Skills';
+import { Projects } from './components/Projects';
+import { About } from "./components/About";
+import { ContactCard } from './components/ContactCard'; 
+import {Footer} from './components/Footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+  return(
+    <div className='App'>
+      <NavBar />
+      <Banner/>
+      <Skills/>
+      <Projects/>
+      <About/>
+      <ContactCard/>
+      <Footer/>
     </div>
-  );
+  )
 }
 
 export default App;
